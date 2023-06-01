4d151865cfa3d93343e2c8fe138364c0baa430eb
Antonin <85553842+AntoninMart@users.noreply.github.com>
2023-06-01 11:27:28 +0200

    ajout de la section carte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MapComponent } from './components/map/map.component';
+import { AboutComponent } from './components/about/about.component';
+import { CarteComponent } from './components/carte/carte.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
+    MapComponent,
+    AboutComponent,
+    CarteComponent,
   ],
   imports: [
     BrowserModule,
