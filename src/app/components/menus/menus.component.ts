b2ab1261c7bc4ebc29a560b47432c55b656a2516
Antonin <85553842+AntoninMart@users.noreply.github.com>
2023-06-07 17:40:23 +0200

    navigation menus fonctionnel

--- src/app/components/menus/menus.component.ts
+++ src/app/components/menus/menus.component.ts
@@ -11,6 +11,8 @@ export class MenusComponent {
   product: any = data;
   categories: any = categories;
   display_product_tab: any[] = [];
+  categories_actuelle: any[] = [];
+
   constructor() {
     let display_name = 'menus';
     this.display_product(display_name);
@@ -18,6 +20,10 @@ export class MenusComponent {
   
   display_product(display_name: string) {
     console.log(display_name);
+    console.log(this.categories_actuelle);
+    if (!this.categories_actuelle.includes(display_name) && display_name !== 'undefined') {
+      this.categories_actuelle.push(display_name);
+    }
     if (display_name === 'menus') {
       const products = this.categories['Categories'] || [];
       this.display_product_tab = [];
@@ -48,8 +54,13 @@ export class MenusComponent {
     }
   }
 
-  public function_test(product: any) {
-    console.log(product);
+  get_back() {
+    this.display_product(this.categories_actuelle[this.categories_actuelle.length - 2]);
+    if (this.categories_actuelle.length > 1) {
+      this.categories_actuelle.pop();
+    }
+    console.log('LA LISTE :');
+    console.log(this.categories_actuelle);
   }
 
 }
