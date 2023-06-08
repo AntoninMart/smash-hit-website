038b3e692f6386305d65fd3b195bf5f4acfa529e
Antonin <85553842+AntoninMart@users.noreply.github.com>
2023-06-08 22:03:55 +0200

    un peu de classe

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,11 @@ module.exports = {
         // 'smash-grey': '#343232',
         // 'smash-red': '#D14D36',
         // 'smash-yellow': '#FAB400',
-        'smash-white-moche': '#E0D8C4',
+        'smash-white': '#E0D8C4',
         'smash-grey': '#212121',
         'smash-red': '#d73131',
         'smash-yellow': '#ffab00',
-        'smash-white': '#ffffff',
+        // 'smash-white': '#ffffff',
       },
       screens: {
         'xs': '475px',
