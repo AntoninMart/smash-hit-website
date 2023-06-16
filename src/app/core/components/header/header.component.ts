be742ada4684444dc282597f20dce58b59b22fbb
Antonin <85553842+AntoninMart@users.noreply.github.com>
2023-06-16 18:05:11 +0200

    update

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,12 +1,10 @@
-import { Component, HostListener  } from '@angular/core';
-import { Scroll } from '@angular/router';
+import { Component  } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-
 
 export class HeaderComponent {
   show: boolean = false;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // 'smash-grey': '#343232',
-        // 'smash-red': '#D14D36',
-        // 'smash-yellow': '#FAB400',
         'smash-white': '#E0D8C4',
         'smash-grey': '#212121',
         'smash-red': '#d73131',
         'smash-yellow': '#ffab00',
-        // 'smash-white': '#ffffff',
       },
       screens: {
         'xs': '475px',
